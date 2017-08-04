80cd8810e4c31ce72fa5caf47c6293bbfdc658ec
Konru <konrumi@gmail.com>
2017-08-04 13:14:04 +0800

    update pageName

--- layout/view/layout/head-res.php
+++ layout/view/layout/head-res.php
@@ -1,7 +1,7 @@
 <script>
     window.__pageData = {};
     window.__pageData.bootTime = (new Date()).getTime();
-    window.__pageData.pageName = window.location.pathname;
+    window.__pageData.pageName = 'index';
 </script>
 
 <link rel="stylesheet" href="<? fe('/lib/font-awesome/css/font-awesome.min.css') ?>">
