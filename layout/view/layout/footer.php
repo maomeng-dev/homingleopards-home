65258885fdf33d6d5e79e9bc60bbd629bd956c31
Konru <konrumi@gmail.com>
2017-10-12 23:19:14 +0800

    add footer link layer

--- layout/view/layout/footer.php
+++ layout/view/layout/footer.php
@@ -4,12 +4,11 @@
             <li><a href="#" target="_blank" title="我们是谁" rel="noopener">我们是谁</a></li>
             <li><a href="#" target="_blank" title="联系我们" rel="noopener">联系我们</a></li>
             <li><a href="#" target="_blank" title="友情链接" rel="noopener">友情链接</a></li>
-            <li><a title="微信"><i class="fa fa-weixin" aria-hidden="true"></i></a></li>
-            <li><a href="https://weibo.com/chinesefelid" target="_blank" title="微博" rel="noopener"><i class="fa fa-weibo" aria-hidden="true"></i></a></li>
+            <li><a title="微信公众号" class="footer-layer-btn"><i class="fa fa-weixin" aria-hidden="true"></i> <span class="footer-layer-wrap"><img src="https://picsum.photos/200/300" alt=""></span></a></li>
+            <li><a href="https://weibo.com/chinesefelid" target="_blank" title="微博主页" rel="noopener"><i class="fa fa-weibo" aria-hidden="true"></i></a></li>
         </ul>
         <div class="pull-left footer-copyright">
-            <p><i class="fa fa-code" aria-hidden="true"></i> with
-                <i class="fa fa-heart" aria-hidden="true"></i> by CFCA-Dev-Team.</p>
+            <p><i class="fa fa-code" aria-hidden="true"></i> with<i class="fa fa-heart" aria-hidden="true"></i> by CFCA-Dev-Team.</p>
             <p>CFCA Copyright 2017-<? echo date("o") ?>. All rights reserved.</p>
             <p>京ICP备17044153号</p>
         </div>
