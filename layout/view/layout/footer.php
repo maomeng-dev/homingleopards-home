5f5385e05efbb09434eaf4f518d83bf8c4a19c6c
Konru <konrumi@gmail.com>
2017-10-13 17:06:22 +0800

    change qr pic

--- layout/view/layout/footer.php
+++ layout/view/layout/footer.php
@@ -4,7 +4,7 @@
             <li><a href="#" target="_blank" title="我们是谁" rel="noopener">我们是谁</a></li>
             <li><a href="#" target="_blank" title="联系我们" rel="noopener">联系我们</a></li>
             <li><a href="#" target="_blank" title="友情链接" rel="noopener">友情链接</a></li>
-            <li><a title="微信公众号" class="footer-layer-btn"><i class="fa fa-weixin" aria-hidden="true"></i> <span class="footer-layer-wrap"><img src="https://picsum.photos/200/300" alt=""></span></a></li>
+            <li><a title="微信公众号" class="footer-layer-btn"><i class="fa fa-weixin" aria-hidden="true"></i> <span class="footer-layer-wrap"><img src="<? fe('/img/common/maomeng-qr.png') ?>" alt=""></span></a></li>
             <li><a href="https://weibo.com/chinesefelid" target="_blank" title="微博主页" rel="noopener"><i class="fa fa-weibo" aria-hidden="true"></i></a></li>
         </ul>
         <div class="pull-left footer-copyright">
