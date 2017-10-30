7b8133796de3dab3eefbbb8800284175b8a34a70
Konru <konrumi@gmail.com>
2017-10-31 00:43:11 +0800

    add index

--- layout/view/index/index.php
+++ layout/view/index/index.php
@@ -11,6 +11,11 @@
 
     <? include VIEW_PATH . '/layout/head-res.php' ?>
 
+    <script>
+        window.__pageData.pageName = 'index';
+    </script>
+
+<!--    <link rel="stylesheet" type="text/css" href="--><?// fe('/lib/slick/slick.css') ?><!--" />-->
     <link rel="stylesheet" href="<? fe('/css/index/index.css') ?>">
 </head>
 
@@ -18,16 +23,33 @@
 
 <? include VIEW_PATH . '/layout/header.php' ?>
 
+<!-- content wrap -->
+<div class="content">
 
-<a class="index-coverpic" href="https://weibo.com/ttarticle/p/show?id=2309404098266164782974" target="_blank" title="带豹回家网站即将上线">带豹回家网站即将上线</a>
+    <!-- banner -->
+    <div class="banner">
+        <img src="https://picsum.photos/1440/720/?random&i=0" alt="banner1">
+    </div>
+    <!--/banner -->
 
-<footer>
-    <i class="fa fa-code" aria-hidden="true" style="color: royalblue;"></i> with <i class="fa fa-heart" aria-hidden="true" style="color:indianred;"></i> by CFCA-Dev-Team.&copy; 2017-<? echo date("o") ?> .
-</footer>
+    <!-- intro -->
+    <div class="intro">
+        <h2>中国猫科动物保护联盟（CFCA）简介</h2>
+        <div class="page-wrap">
+            <p>中国猫科动物保护联盟（CFCA）简称猫盟，是由几个生态爱好者和科学家共同成立的民间环保组织，这些年来一直在做一件事：研究和保护中国的野生猫科动动。</p>
+            <p>行走在山林里，猫盟的每一个人都像兽类一样安静，对移动的活物敏感，对各种动物痕迹敏感，摸爬滚打不在话下；离开山林，整理分析数据、写稿编视频、传播、设计制作周边……各有分工，每个人都是一支队伍。以下，逐一介绍一下猫盟的兄弟姐妹。</p>
+        </div>
+    </div>
+
+</div>
+<!--/content wrap -->
+
+<? include VIEW_PATH . '/layout/footer.php' ?>
 
 <? include VIEW_PATH . '/layout/foot-res.php' ?>
 
-<!--<script src="<?/* fe('/js/index/index.js') */?>"></script>-->
+<script src="<? fe('/lib/slick/slick.min.js') ?>"></script>
+<!--<script src="--><?// fe('/js/page/about-north-chinese-leopards.js') ?><!--"></script>-->
 
 </body>
 </html>
---
+++ layout/view/layout/header.php
@@ -0,0 +1,14 @@
+<header class="clearfix">
+    <div class="header-logo">CFCA</div>
+    <ul class="header-menu">
+        <li><a class="active" href="#" title="猫盟介绍">猫盟介绍</a></li>
+        <li><a href="#" title="我们在募捐">我们在募捐</a></li>
+        <li><a href="#" title="文章文章">文章文章</a></li>
+        <li><a href="#" title="活动文章">活动文章</a></li>
+        <li><a href="#" title="资源站">资源站</a></li>
+        <li><a href="#" title="影像">影像</a></li>
+        <li><a href="#" title="加入我们">加入我们</a></li>
+        <li><a title="微信公众号" class="header-layer-btn"><i class="fa fa-weixin" aria-hidden="true"></i> <span class="header-layer-wrap"><img src="<? fe('/img/common/maomeng-qr.png') ?>" alt=""></span></a></li>
+        <li><a href="https://www.weibo.com/chinesefelid" target="_blank" title="微博主页" rel="noopener"><i class="fa fa-weibo" aria-hidden="true"></i></a></li>
+    </ul>
+</header>
