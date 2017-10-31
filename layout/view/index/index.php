a637a9a05440342eb0349cdf216f9a3d38ecf8d0
Konru <konrumi@gmail.com>
2017-11-01 00:13:37 +0800

    update index

--- layout/view/index/index.php
+++ layout/view/index/index.php
@@ -34,13 +34,145 @@
 
     <!-- intro -->
     <div class="intro">
-        <h2>中国猫科动物保护联盟（CFCA）简介</h2>
-        <div class="page-wrap">
-            <p>中国猫科动物保护联盟（CFCA）简称猫盟，是由几个生态爱好者和科学家共同成立的民间环保组织，这些年来一直在做一件事：研究和保护中国的野生猫科动动。</p>
-            <p><br></p>
-            <p>行走在山林里，猫盟的每一个人都像兽类一样安静，对移动的活物敏感，对各种动物痕迹敏感，摸爬滚打不在话下；离开山林，整理分析数据、写稿编视频、传播、设计制作周边……各有分工，每个人都是一支队伍。以下，逐一介绍一下猫盟的兄弟姐妹。</p>
+        <div class="intro-block intro-organization">
+            <h2>中国猫科动物保护联盟（CFCA）简介</h2>
+            <div class="page-wrap">
+                <p>中国猫科动物保护联盟（CFCA）简称猫盟，是由几个生态爱好者和科学家共同成立的民间环保组织，这些年来一直在做一件事：研究和保护中国的野生猫科动动。</p>
+                <p><br></p>
+                <p>行走在山林里，猫盟的每一个人都像兽类一样安静，对移动的活物敏感，对各种动物痕迹敏感，摸爬滚打不在话下；离开山林，整理分析数据、写稿编视频、传播、设计制作周边……各有分工，每个人都是一支队伍。以下，逐一介绍一下猫盟的兄弟姐妹。</p>
+            </div>
+        </div>
+
+        <div class="intro-block intro-primer">
+            <div class="page-wrap">
+                <p>离开山林，整理分析数据、写稿编视频、传播、设计制作周边……各有分工，每个人都是一支队伍。以下，逐一介绍一下猫盟的兄弟姐妹。</p>
+                <p class="text-right">——猫克·吐奶</p>
+            </div>
+        </div>
+
+        <div class="intro-block intro-member">
+            <div class="page-wrap">
+                <p>行走在山林里，猫盟的每一个人都像兽类一样安静，对移动的活物敏感，对各种动物痕迹敏感，摸爬滚打不在话下；离开山林，整理分析数据、写稿编视频、传播、设计制作周边……各有分工，每个人都是一支队伍。</p>
+                <p class="text-right"><a href="#">更多内容...</a></p>
+            </div>
         </div>
     </div>
+    <!--/intro-->
+
+    <!-- contribute -->
+    <div class="contribute">
+        <h2>您也可以贡献一份力量</h2>
+        <p>我们在腾讯公益发起了募捐，期待您的加入</p>
+        <a>查看实时进度</a>
+    </div>
+    <!--/contribute -->
+
+    <!-- gallery -->
+    <div class="gallery">
+        <h2>了解更多信息，更懂动物保护</h2>
+        <ul class="gallery-list">
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=0" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=1" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=2" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=3" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=4" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=5" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=6" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=7" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=8" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=9" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=10" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=11" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=12" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+            <li class="gallery-item">
+                <img src="https://picsum.photos/185/185/?random&gallery=13" alt="图片标题&#13;Binomial Nomenclature">
+                <div class="gallery-item-desc" title="图片标题&#13;Binomial Nomenclature">
+                    <span class="gallery-item-title">图片标题</span>
+                    <span class="gallery-item-text">Binomial Nomenclature</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!--/gallery -->
+
 
 </div>
 <!--/content wrap -->
