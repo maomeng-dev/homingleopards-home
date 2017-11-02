c5c466a8f5f33facc921ddf18a88a74ba184428b
Konru <konrumi@gmail.com>
2017-11-02 23:08:03 +0800

    add scroll trigger module

---
+++ layout/src/js/common/scrollTrigger.module.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let passive = require('./passive.module');
+
+let $doc = $(document);
+
+
+let scrollTrigger = {
+    init: function(conf) {
+        conf = conf || {};
+        this.delay = conf.delay || 100;
+        this.distance = conf.distance || 0;
+
+        let scrollTimer = 0;
+
+        passive.addPassiveEventListener($doc[0], 'scroll resize', () => {
+            clearTimeout(scrollTimer);
+            scrollTimer = setTimeout(() => {
+                this.check();
+            }, this.delay);
+        });
+
+        this.check();
+    },
+
+    check: function() {
+        $.each($('[data-scroll]'), (index, element) => {
+            let distance = this.distance || (window.innerHeight / 3);
+            if (element.getBoundingClientRect().top - window.innerHeight < -distance) {
+                this.trigger(element);
+            }
+        });
+    },
+
+    trigger: function(element) {
+        let $ele = $(element);
+        $ele
+            .addClass('scroll-triggered')
+            .removeAttr('data-scroll');
+
+        $.each($ele.find('[data-scroll-child]'), (index, childElement) => {
+            let $childElement = $(childElement);
+            let durationTime = $childElement.attr('data-scroll-child') || 0;
+
+            setTimeout(() => {
+                $childElement.addClass('scroll-triggered');
+            }, durationTime);
+        });
+    }
+};
+
+module.exports = scrollTrigger;
