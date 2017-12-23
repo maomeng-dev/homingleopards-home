422f9ecf67fc1848c7d93f65fc2d8f4a49bddeb9
Konru <konrumi@gmail.com>
2017-12-23 09:48:45 +0800

    :sparkles: Add share track

--- layout/view/layout/foot-res.php
+++ layout/view/layout/foot-res.php
@@ -8,28 +8,44 @@
 <script>
     var _hmt = _hmt || [];
     (function () {
+        /* track */
+        var hm = document.createElement('script');
+        hm.src = 'https://hm.baidu.com/hm.js?bd019c4b798c9f99f8da9fa737c6e5a8';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(hm, s);
+
         /* share */
         wx.config(<? echo shareJssdk(array('onMenuShareAppMessage', 'onMenuShareTimeline')) ?>);
         wx.ready(function () {
             wx.onMenuShareTimeline({
                 title: window.__shareConfig.title,
                 link: window.__shareConfig.link,
-                imgUrl: window.__shareConfig.imgUrl
+                imgUrl: window.__shareConfig.imgUrl,
+                success: function () {
+                    _hmt.push([
+                        '_trackEvent',
+                        window.__pageData.pageName,
+                        'actoin-share',
+                        'wx-timeline'
+                    ]);
+                }
             });
 
             wx.onMenuShareAppMessage({
                 title: window.__shareConfig.title,
                 desc: window.__shareConfig.desc,
                 link: window.__shareConfig.link,
-                imgUrl: window.__shareConfig.imgUrl
+                imgUrl: window.__shareConfig.imgUrl,
+                success: function () {
+                    _hmt.push([
+                        '_trackEvent',
+                        window.__pageData.pageName,
+                        'actoin-share',
+                        'wx-message'
+                    ]);
+                }
             });
         });
-
-        /* track */
-        var hm = document.createElement('script');
-        hm.src = 'https://hm.baidu.com/hm.js?bd019c4b798c9f99f8da9fa737c6e5a8';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(hm, s);
 
         $(document)
             .on('DOMContentLoaded', function () {
