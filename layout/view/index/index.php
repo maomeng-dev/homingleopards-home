7f1772832adf1c097ca4d2c1399603a0fa15de31
Mi Qimeng <konrumi@gmail.com>
2017-12-25 11:47:41 +0800

    :chart_with_upwards_trend: Add baidu zhanzhang verify meta tag

--- layout/view/index/index.php
+++ layout/view/index/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="baidu-site-verification" content="zXnVFcidr1" />
 
     <link rel="icon" href="<? fe('/favicon.png') ?>">
 
