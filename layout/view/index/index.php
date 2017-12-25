37a995e1a8cf0d18d66b07600e52b5697d858e4a
Mi Qimeng <konrumi@gmail.com>
2017-12-25 12:50:18 +0800

    :alien: Update baidu site verify code

--- layout/view/index/index.php
+++ layout/view/index/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="baidu-site-verification" content="zXnVFcidr1" />
+    <meta name="baidu-site-verification" content="v0Dp6gUvky" />
 
     <link rel="icon" href="<? fe('/favicon.png') ?>">
 
