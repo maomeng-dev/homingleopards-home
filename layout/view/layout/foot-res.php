ad127b8d9465fc1bbcf97cdc1e9ee113703bf43d
Mi Qimeng <konrumi@gmail.com>
2017-12-25 11:53:15 +0800

    :chart_with_upwards_trend: Add auto push to baidu

--- layout/view/layout/foot-res.php
+++ layout/view/layout/foot-res.php
@@ -14,6 +14,18 @@
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(hm, s);
 
+        /* push */
+        var bp = document.createElement('script');
+        var curProtocol = window.location.protocol.split(':')[0];
+        if (curProtocol === 'https') {
+            bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+        }
+        else {
+            bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+        }
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(bp, s);
+
         /* share */
         wx.config(<? echo shareJssdk(array('onMenuShareAppMessage', 'onMenuShareTimeline')) ?>);
         wx.ready(function () {
