045dec5eb98cfb7afb6e523ec4fce07562313cf8
Mi Qimeng <konrumi@gmail.com>
2018-01-05 18:56:59 +0800

    :lipstick: Update share styles

--- layout/view/layout/sidebar.php
+++ layout/view/layout/sidebar.php
@@ -1,6 +1,6 @@
 <section class="sidebar" id="sidebar">
-    <a class="share-btn" title="分享到微信" data-role="share-wx"><i class="fa fa-weixin fa-2x" aria-hidden="true"></i></a>
+    <a class="share-btn" title="分享到微信" data-role="share-wx"><i class="sidebar-icon icon-wx"></i></a>
     <span class="share-img" data-role="share-wx-img" style="right: -160px;"><span class="img" data-role="share-wx-qr"></span><span class="text text-center">扫码在微信打开</span></span>
-    <a class="share-btn" title="分享到微博" data-role="share-wb"><i class="fa fa-weibo fa-2x" aria-hidden="true"></i></a>
-    <a class="share-btn" title="返回顶部" data-role="to-top" style="display: none;"><i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i></a>
+    <a class="share-btn" title="分享到微博" data-role="share-wb"><i class="sidebar-icon icon-wb"></i></a>
+    <a class="share-btn" title="返回顶部" data-role="to-top" style="display: none;"><i class="sidebar-icon icon-top"></i></a>
 </section>
