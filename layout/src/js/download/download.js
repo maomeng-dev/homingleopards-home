3dd6ad30f6debc047f374d76bd8a8f6abcca6dbe
Mi Qimeng <konrumi@gmail.com>
2018-03-30 19:03:53 +0800

    :sparkles: Add download pages

---
+++ layout/src/js/download/download.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let totop = require('../common/totop.module');
+
+$(() => {
+    let page = {
+        eles: {
+        },
+
+        init: function () {
+            totop.init();
+        }
+    };
+
+    page.init();
+});
