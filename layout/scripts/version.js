9093aa0512bfccc4d9d8b6be3603559e10175366
konrumi <konrumi@gmail.com>
2018-07-26 11:06:37 +0800

    :wrench: Update js build version script timestamp to second

--- layout/scripts/version.js
+++ layout/scripts/version.js
@@ -2,10 +2,10 @@ let fs = require('fs');
 
 let _config = require('../config/_config');
 
-let currentVersion = (new Date()).getTime().toString();
+let currentVersion = ((new Date()).getTime() / 1000).toFixed(0);
 
-fs.writeFile(`${_config.projectRoot}/VERSION.txt`, currentVersion, function(err) {
-    if(err) {
+fs.writeFile(`${_config.projectRoot}/VERSION.txt`, currentVersion, function (err) {
+    if (err) {
         return console.log(err);
     }
 
