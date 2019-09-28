e7e60ccb8ad36299a79cc31483ba027d6267acea
konrumi <konrumi@gmail.com>
2019-09-28 19:29:14 +0800

    :bug: fix resize event

--- layout/src/js/common/lazyload.module.js
+++ layout/src/js/common/lazyload.module.js
@@ -1,47 +1,54 @@
-'use strict';
+'use strict'
 
-let passive = require('./passive.module');
+let passive = require('./passive.module')
 
-let $doc = $(document);
+let $doc = $(document)
 
 let lazyload = {
-    init: function (conf) {
-        conf = conf || {};
-        this.delay = conf.delay || 100;
-        this.distance = conf.distance || 100;
+  init: function (conf) {
+    conf = conf || {}
+    this.delay = conf.delay || 100
+    this.distance = conf.distance || 100
 
-        let scrollTimer = 0;
+    let scrollTimer = 0
 
-        passive.addPassiveEventListener($doc[0], 'scroll resize', () => {
-            clearTimeout(scrollTimer);
-            scrollTimer = setTimeout(() => {
-                this.lazy();
-            }, this.delay);
-        });
-
-        this.lazy();
-    },
-
-    lazy: function () {
-        $.each($('[data-lazysrc]'), (index, element) => {
-            this.check(element);
-        });
-    },
-
-    check: function (element) {
-        if (element.getBoundingClientRect().top - window.innerHeight < this.distance) {
-            this.load(element);
-        }
-    },
-
-    load: function (element) {
-        let $ele = $(element);
-        $ele
-            .attr({
-                src: $ele.attr('data-lazysrc')
-            })
-            .removeAttr('data-lazysrc');
+    let callCheck = () => {
+      clearTimeout(scrollTimer)
+      scrollTimer = setTimeout(() => {
+        this.lazy()
+      }, this.delay)
     }
-};
 
-module.exports = lazyload;
+    passive.addPassiveEventListener($doc[0], 'scroll', () => {
+      callCheck()
+    })
+    passive.addPassiveEventListener(window, 'resize', () => {
+      callCheck()
+    })
+
+    this.lazy()
+  },
+
+  lazy: function () {
+    $.each($('[data-lazysrc]'), (index, element) => {
+      this.check(element)
+    })
+  },
+
+  check: function (element) {
+    if (element.getBoundingClientRect().top - window.innerHeight < this.distance) {
+      this.load(element)
+    }
+  },
+
+  load: function (element) {
+    let $ele = $(element)
+    $ele
+        .attr({
+          src: $ele.attr('data-lazysrc')
+        })
+        .removeAttr('data-lazysrc')
+  }
+}
+
+module.exports = lazyload
--- layout/src/js/common/totop.module.js
+++ layout/src/js/common/totop.module.js
@@ -1,77 +1,80 @@
-'use strict';
+'use strict'
 
-let passive = require('./passive.module');
+let passive = require('./passive.module')
 
-let $doc = $(document);
-let $body = $('html, body');
+let $doc = $(document)
+let $body = $('html, body')
 
 let totop = {
-    init: function (conf) {
-        conf = conf || {};
-        this.wrapWidth = conf.wrapWidth || 1440;
-        this.className = conf.className || 'page-totop';
-        this.duration = conf.duration || 500;
+  init: function (conf) {
+    conf = conf || {}
+    this.wrapWidth = conf.wrapWidth || 1440
+    this.className = conf.className || 'page-totop'
+    this.duration = conf.duration || 500
 
-        this.template = '<a class="page-totop" style="display: none;"><i class="fa fa-chevron-up" aria-hidden="true"></i><span>返回顶部</span></a>';
-        this.element = $(conf.element) || $(ejs.render(this.template));
-        this.isShowing = false;
+    this.template = '<a class="page-totop" style="display: none;"><i class="fa fa-chevron-up" aria-hidden="true"></i><span>返回顶部</span></a>'
+    this.element = $(conf.element) || $(ejs.render(this.template))
+    this.isShowing = false
 
-        passive.addPassiveEventListener($doc[0], 'scroll resize', () => {
-            this.check();
-        });
+    passive.addPassiveEventListener($doc[0], 'scroll', () => {
+      this.check()
+    })
+    passive.addPassiveEventListener(window, 'resize', () => {
+      this.check()
+    })
 
-        this.element.on('click', function () {
-            $body.stop().animate({
-                scrollTop: 0
-            }, 500);
-        });
+    this.element.on('click', function () {
+      $body.stop().animate({
+        scrollTop: 0
+      }, 500)
+    })
 
-        if (!conf.element) {
-            $body.append(this.element);
-        }
-
-        this.check();
-    },
-
-    check: function () {
-        if (this.wrapWidth > window.innerWidth) {
-            this.hide();
-        } else {
-            if ($doc.scrollTop() > window.innerHeight) {
-                this.show();
-            } else {
-                this.hide();
-            }
-        }
-    },
-
-    show: function () {
-        if (!this.isShowing) {
-            this.element
-                .stop()
-                .css({
-                    'display': 'block',
-                    'opacity': '0'
-                })
-                .animate({
-                    'opacity': '1'
-                }, 200);
-            this.isShowing = true;
-        }
-    },
-
-    hide: function () {
-        if (this.isShowing) {
-            this.element
-                .stop()
-                .animate({
-                    'opacity': '0'
-                }, 200, () => {
-                    this.element.css({ 'display': 'none' });
-                });
-            this.isShowing = false;
-        }
+    if (!conf.element) {
+      $body.append(this.element)
     }
-};
 
-module.exports = totop;
+    this.check()
+  },
+
+  check: function () {
+    if (this.wrapWidth > window.innerWidth) {
+      this.hide()
+    } else {
+      if ($doc.scrollTop() > window.innerHeight) {
+        this.show()
+      } else {
+        this.hide()
+      }
+    }
+  },
+
+  show: function () {
+    if (!this.isShowing) {
+      this.element
+          .stop()
+          .css({
+            'display': 'block',
+            'opacity': '0'
+          })
+          .animate({
+            'opacity': '1'
+          }, 200)
+      this.isShowing = true
+    }
+  },
+
+  hide: function () {
+    if (this.isShowing) {
+      this.element
+          .stop()
+          .animate({
+            'opacity': '0'
+          }, 200, () => {
+            this.element.css({ 'display': 'none' })
+          })
+      this.isShowing = false
+    }
+  }
+}
+
+module.exports = totop
